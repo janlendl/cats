a26dc18eaa890b50b69fcec8a3142b1935890200
Lendl Peralta <janlendl@gmail.com>
2021-08-15 04:31:50 +0000

    corrected return with callback and created new function

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 
-const breedDetailsFormFile = function(breed) {
+const breedDetailsFormFile = function(breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log('In readFile\'s Callback: it has the data.');
-    // ISSUE: Returning from *inner callback function, not breedDetailsFromFile.
-    if (!error) return data;
+    // CHANGE: Pass data into callback instead of returning it directly
+    if (!error) callback(data);
   });
-  // ISSUE: Attempting to return data out here will also not work.
-  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
-// we try to get the return value
-const bombay = breedDetailsFormFile('Bombay');
-console.log('Return Value', bombay); // will NOT print out details, instead we will see un
+  // CHANGE 1: Moved the console.log into a new function:
+const printCatBreed = breed => {
+  console.log('Return Value: ', breed); // print out details correctly  
+};
+// CHANGE 2: we're now processing two arguments into breedDetailsFormFile: breed string and a callback function
+breedDetailsFormFile('Bombay', printCatBreed);
